88edbadb282e8252e61ede9d0b4f3a077b6cccd6
AndrePedreschi <a.pedreschi32@gmail.com>
2021-11-17 23:20:19 -0300

    17/11/2021

--- 2-Programação Imperativa/Aula11/Mesa.js
+++ 2-Programação Imperativa/Aula11/Mesa.js
@@ -12,11 +12,45 @@ function uper (upper){
 
 //2.
 function passagemDeElementos (x, y){
-    for(let j=0; j<x.length; j++){
-        
-        x[j] = x[j].push(y[j].pop().toUpperCase());     
+    const qtd = x.length;
+    for(let j=0; j<qtd; j++){
+        x.push(y.pop().toUpperCase());     
     }
     return x
     
 }
-console.log(passagemDeElementos(filmes, filmesAnimacao));
+//console.log(passagemDeElementos(filmes, filmesAnimacao));
+
+
+//3.
+function passagemDeElementos2 (x, y){
+    let invalido = y.pop();
+    const qtd = y.length;
+    console.log("esse filme é invalido ",invalido);
+    for(let j=0; j<qtd; j++){
+        x.push(y.pop().toUpperCase());     
+    }
+    return x
+    
+}
+//console.log(passagemDeElementos2(filmes, filmesAnimacao));
+
+
+//4.
+//Crie uma função que compare as notas e nos diga se elas são iguais ou diferentes.
+//Para verificar se tudo está bem até agora, recomendamos testar cada uma das funções, a fim de verificar seu correto funcionamento.
+//Se chegarmos a este ponto e tudo estiver bem, o tech leader da equipe deve estar extremamente feliz com nosso trabalho e desempenho.
+const asiaScores = [8, 10, 6, 9, 10, 6, 6, 8, 4];
+const euroScores = [8, 10, 6, 8, 10, 6, 7, 9, 5];
+
+function compararQualificacoes (a,b){
+    let compararAsiaEuro = [];
+    for(let cont=0; cont<asiaScores.length; cont++){
+        compararAsiaEuro[cont] = a[cont] === b[cont];
+    }
+    return compararAsiaEuro
+}
+
+//console.log(compararQualificacoes(asiaScores, euroScores));
+
+
---
+++ 2-Programação Imperativa/Aula12/Mesa.js
@@ -0,0 +1,50 @@
+const alicia = [23, 69, 32];
+const bob = [12, 67, 43];
+
+
+
+function encontraGanhador (a,b){
+    let pontosPrimeiroParticipante = 0;
+    let pontosSegundoParticipante = 0;
+
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > b[i]) {
+            pontosPrimeiroParticipante++;
+        } else if (b[i] > a[i]) {
+            pontosSegundoParticipante++;
+        }
+
+    }
+    console.log(pontosPrimeiroParticipante);
+    console.log(pontosSegundoParticipante);
+
+    if (pontosPrimeiroParticipante>pontosSegundoParticipante) {
+        return "Alicia"
+    }else{
+        return "Bob"
+    }
+
+}
+//console.log("O vencedor é: "+ encontraGanhador(alicia,bob));
+
+
+function digitalHouse (num1, num2){
+
+    for(let j=1; j<=100; j++){
+        if (j%num1==0 & j%num2==0) {
+            console.log("O número "+j+" é Digital House")
+        }else if (j%num1==0) {
+            console.log("O número "+j+" é Digital");
+        }else if(j%num2==0){
+            console.log("O número "+j+" é House");
+        }
+
+    }
+
+}
+//console.log(digitalHouse(1,7));
+
+
+
+
+
