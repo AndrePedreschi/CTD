8da531ad092e643065cd4342e7949d5d43373c39
AndrePedreschi <a.pedreschi32@gmail.com>
2021-11-28 18:11:37 -0300

    checkpoint-2 de front end

---
+++ 2-Front-End I/Checkpoint - 2/README.md
@@ -0,0 +1,1 @@
+# front_checkpoint_2
---
+++ 2-Programação Imperativa/Checkpoint-2/checkpoint.js
@@ -0,0 +1,81 @@
+//1
+// var z=0;
+// for(var i=20; i<50;i+=10){
+//     z+=i;
+// }
+// console.log(z);
+
+
+
+
+//2
+// function soma(a=1,b=3) {
+//     return a+b;
+// }
+
+// console.log(soma());
+// console.log(soma(3));
+// console.log(soma(1,2));
+
+
+
+//3 
+
+// function soletrar(texto) {
+//     console.log(texto.replace('-','').toLocaleUpperCase().split("").join("-"));
+// }
+// soletrar("digital-house");
+// soletrar("ctd");
+
+
+//4
+// let numeros=[10,8,7,99,0,1,6];
+// numeros.sort(function(a,b) {
+//     return b-a;
+// });
+// console.log(numeros);
+
+
+
+//5
+
+// for(var i=0; i<50; i+=10){
+// console.log(i);
+
+// }
+// console.log(i);
+
+
+//6
+
+// let x=5;
+// let y=x++ + ++x;
+// console.log("y= "+y);
+// x=3;
+// y=x*(x+1)*x++;
+// x=5;
+// y=3;
+// y*=x+1
+// console.log("x= "+x);
+// console.log("y= "+y);
+
+
+
+//7
+// let valor=5;
+// let fatorial=1;
+// for (let i = valor; i > 1; i--) {
+//     fatorial*=i;   
+// }
+// console.log("Fatorial= "+fatorial);
+
+
+// let numberStore = [0, 1, 2];
+// let newNumber = 12;
+// numberStore = [numberStore, newNumber];
+
+// console.log(numberStore);
+
+
+
+
