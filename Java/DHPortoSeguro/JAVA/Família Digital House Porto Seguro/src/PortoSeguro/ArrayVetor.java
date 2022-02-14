32ec54145d686a10da335877d9ff6fb030414630
AndrePedreschi <a.pedreschi32@gmail.com>
2022-02-14 08:52:25 -0300

    atualização 14/02/2022

---
+++ Java/DHPortoSeguro/JAVA/Família Digital House Porto Seguro/src/PortoSeguro/ArrayVetor.java
@@ -0,0 +1,26 @@
+package PortoSeguro;
+
+import java.util.Scanner;
+
+public class ArrayVetor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float[] media = new float[5];
+		float n1,n2,n3,somaMedia=0,mediaGeral;
+		int x;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		for(x=0;x<=5;x++);
+		
+		
+			
+		
+			
+		
+
+	}
+
+}
---
+++ Java/DHPortoSeguro/JAVA/Família Digital House Porto Seguro/src/PortoSeguro/OlaMundo.java
@@ -0,0 +1,32 @@
+package PortoSeguro;
+
+import java.util.Scanner;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nome; //estou criando uma variável do tipo string
+		int idade=29;
+		float altura;
+		
+		Scanner leia = new Scanner(System.in); //estou crindo a entreada de dados
+		
+		System.out.println("Olá mundo!!!!");
+		
+		System.out.println("\n\t\tEntre com o seu nome:");
+		nome = leia.nextLine();
+		System.out.println("\n\t\tEntre com a sua altura: ");
+		altura = leia.nextFloat();
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
