23a68edb57851f7d644c8a5e4e2de2dd252e5ece
AndrePedreschi <a.pedreschi32@gmail.com>
2022-04-13 20:11:01 -0300

    Atualizado

--- 3-Front-End II/Aula18-21/scripts/tarefas/tarefas.js
+++ 3-Front-End II/Aula18-21/scripts/tarefas/tarefas.js
@@ -10,7 +10,7 @@ onload = () =>{
     //console.log(tokenJwtusuarioLogado);
 }
 
-console.log(tokenJwtusuarioLogado);
+//console.log(tokenJwtusuarioLogado);
 
 
 
@@ -57,3 +57,145 @@ fetch(endPointUsuario, configuracaoRequisicao).then(resultado=>{
 );
  
     
+function finalizarSessao(){
+    localStorage.removeItem('jwt')
+    alert("Logout Realizado")
+    window.location.href = "index.html"
+}
+
+
+
+//Criar uma Tarefa
+let tarefaJson={
+    "description": "Aprender Front end III",
+    "completed": false
+  }
+let tarefa = JSON.stringify(tarefaJson)
+  let endPointRegistrarTarefas = "https://ctd-todo-api.herokuapp.com/v1/tasks";
+
+  let configuracaoRegistrarTarefas = {
+      method: 'POST',
+      body: tarefa,
+      headers: {
+        'authorization': tokenJwtusuarioLogado,
+        'content-type': 'application/json'
+          
+          }   
+  }
+  
+  fetch(endPointRegistrarTarefas, configuracaoRegistrarTarefas).then(resultado=>{
+          /* if(resultado.status == 201){
+              return resultado.json();
+          }
+          throw resultado; */
+          
+         return resultado.json();      
+      
+      
+      }).then(resultado =>{
+          /* cadastroSucesso(resultado.jwt) */
+          //let resultadoTarefa = resultado;
+          console.log(resultado);
+          //console.log(resultado.id);
+          //console.log(resultado.firstName);
+          //console.log(resultado.lastName);
+          //console.log(resultado.email);
+          //document.querySelector("#nome").innerText = "Bem vindo " + resultado.firstName +" "+resultado.lastName;
+          
+      }).catch(erro=>{
+          /* cadastroErro(erro)
+          if(erro.status==400){
+              alert("Usuário já cadastrado")
+                      
+          } */
+          console.log(erro);
+    }
+);
+
+
+
+
+
+function addTarefas(){
+    let skeleton = document.getElementById('skeleton');
+    let novaTarefa = document.getElementById('novaTarea').innerText;
+
+    let tarefaADD = `
+    <li class="tarefa">
+        <div class="not-done"></div>
+        <div class="descricao">
+            <p class="nome">${novaTarefa}</p>
+            <p class="timestamp">Criada em: ${resultadoTarefa.createdAT}</p>
+        </div>
+    </li>      
+    `
+    
+    skeleton.appendChild(tarefaADD);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //Listar tarefas do usuário
+
+let endPointTarefas = "https://ctd-todo-api.herokuapp.com/v1/tasks";
+
+let configuracaoTarefas = {
+    method: 'GET',
+    //body: usuarioJson,
+    headers: {
+        //'content-type': 'application/json'
+        'authorization': tokenJwtusuarioLogado
+        }   
+}
+
+fetch(endPointTarefas, configuracaoRequisicao).then(resultado=>{
+        /* if(resultado.status == 201){
+            return resultado.json();
+        }
+        throw resultado; */
+        
+       return resultado.json();      
+    
+    
+    }).then(resultado =>{
+        /* cadastroSucesso(resultado.jwt) */
+        console.log(resultado);
+        //console.log(resultado.id);
+        //console.log(resultado.firstName);
+        //console.log(resultado.lastName);
+        //console.log(resultado.email);
+        //document.querySelector("#nome").innerText = "Bem vindo " + resultado.firstName +" "+resultado.lastName;
+        
+    }).catch(erro=>{
+        /* cadastroErro(erro)
+        if(erro.status==400){
+            alert("Usuário já cadastrado")
+                    
+        } */
+        console.log(erro);
+    }
+);
+
+
+
+
+
